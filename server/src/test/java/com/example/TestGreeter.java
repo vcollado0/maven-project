c3288ac064bd0cfec516b399dc7a4bc576af29ed
Victor Collado <31104410+vcollado0@users.noreply.github.com>
2018-08-15 03:07:53 -0400

    fix for test
    testing jenkins

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
 
-#Java unittest being check by jenkins. Fck yeah!
 public class TestGreeter {
 
   private Greeter greeter;
